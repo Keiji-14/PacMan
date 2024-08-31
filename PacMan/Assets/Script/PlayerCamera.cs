b29fbf4ce1b7e623c3d5d86c16a4b5cb7e396b5e
足利 慶次 <a.keiji14@gmail.com>
2024-08-31 17:03:25 +0900

    プレイヤーの移動処理を追加

---
+++ PacMan/Assets/Script/PlayerCamera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        #region SerializeField
+        /// <summary>マウス感度<summary>
+        [SerializeField] private float mouseSensitivity = 100.0f;
+        /// <summary>プレイヤーのTransform</summary>
+        [SerializeField] private Transform playerBody;
+        #endregion
+
+        #region UnityEvent
+        private void Update()
+        {
+            LookAround();
+        }
+        #endregion
+
+        #region PrivateMethod
+        /// <summary>
+        /// マウス入力に基づいてカメラを回転させる処理
+        /// </summary>
+        private void LookAround()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+            // プレイヤーをY軸周りに回転させる
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+        #endregion
+    }
+}
---
+++ PacMan/Assets/Script/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region PrivateField
+        /// <summary>速度ベクトル</summary>
+        private Vector3 velocity;
+        #endregion
+
+        #region SerializeField
+        /// <summary>移動速度</summary>
+        [SerializeField] private float speed;
+        [Header("Component")]
+        /// <summary>アニメーター</summary>
+        [SerializeField] private Animator animator;
+        /// <summary>キャラクターコントローラー</summary>
+        [SerializeField] private CharacterController characterController;
+        #endregion
+
+        #region UnityEvent
+        private void Update()
+        {
+            Move();
+        }
+        #endregion
+
+        /// <summary>
+        /// 移動処理
+        /// </summary>
+        private void Move()
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * moveX + transform.forward * moveZ;
+            characterController.Move(move * speed * Time.deltaTime);
+
+            characterController.Move(velocity * Time.deltaTime);
+        }
+    }
+}
